b194c90d50ac6d6acede2c1dfb89bf2fe17d8023
shubh <shubh.35209@stu.upes.ac.in>
2026-09-07 01:47:50 +0530

    D

---
+++ Q56.c
@@ -0,0 +1,37 @@
+/*
+Q56: Read and print elements of a one-dimensional array.
+
+
+Sample Test Cases:
+Input 1:
+3
+10 20 30
+Output 1:
+10 20 30
+
+Input 2:
+5
+1 2 3 4 5
+Output 2:
+1 2 3 4 5
+
+*/
+#include <stdio.h>
+int main()
+{
+    int arr[100], n, i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("The elements of the array are: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
